00266ffe97d119b0ede52fb68b30858687cbda4a
sakth1x02 <sakthishasumathi2002@gmail.com>
2025-07-01 15:28:17 +0530

    two

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const port = process.env.PORT || 3200;
-const logger = require('./utils/logger'); // Import logger
-
-app.listen(port, () => {
-  logger.info(`Server is running on port http://localhost:${port}`);
-});
